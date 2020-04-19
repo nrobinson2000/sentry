42bf68f44a0b1bcc6fef838898fa4dcfaf07c623
Nathan Robinson <nrobinson2000@me.com>
2020-04-19 19:59:09 +0100

    add ui.py

---
+++ ui.py
@@ -0,0 +1,45 @@
+import curses
+
+from time import sleep
+
+from sentry import sentry
+
+cur = curses.initscr() # initialisation
+
+curses.noecho()
+curses.cbreak()
+cur.keypad(True)
+
+
+def move(sentrycmd, delay):
+    sentrycmd()
+    sleep(delay)
+    sentry.stop()
+
+try:
+     while True:
+        char = cur.getch()
+
+        cur.clear()
+
+        if char == 259:
+            cur.addstr("UP")
+            move(sentry.up, 0.25)
+        if char == 258:
+            cur.addstr("DOWN")
+            move(sentry.down, 0.25)
+        if char == 260:
+            cur.addstr("LEFT")
+            move(sentry.left, 0.25)
+        if char == 261:
+            cur.addstr("RIGHT")
+            move(sentry.right, 0.25)
+
+except KeyboardInterrupt:
+    print("interrupted...")
+
+curses.nocbreak()
+cur.keypad(False)
+curses.echo()
+curses.endwin() # cleanup
+print("Goodbye :-)")
