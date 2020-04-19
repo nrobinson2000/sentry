7276fe6a7a31a1663d14e20e100ed22294049dad
Nathan Robinson <nrobinson2000@me.com>
2020-04-20 00:30:21 +0100

    Improve web app

---
+++ app/app.py
@@ -0,0 +1,42 @@
+from sentry import sentry
+from flask import Flask, request, render_template
+
+DIRECTIONS = ['left', 'right', 'up', 'down']
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/api/move', methods=['POST'])
+def move():
+    direction = request.args.get('direction')
+
+    if direction not in DIRECTIONS:
+        return '', 400
+
+    if direction == 'left':
+        sentry.left()
+    elif direction == 'right':
+        sentry.right()
+    elif direction == 'up':
+        sentry.up()
+    elif direction == 'down':
+        sentry.down()
+
+    return ''
+
+@app.route('/api/stop', methods=['POST'])
+def stop():
+    sentry.stop()
+
+    return ''
+
+@app.route('/api/fire', methods=['POST'])
+def fire():
+    sentry.fire()
+
+    return ''
+
+app.run('0.0.0.0', debug=True)
---
+++ app/static/sentry.js
@@ -0,0 +1,136 @@
+{
+  'use strict'
+
+  const { fetch } = window
+
+  const image = document.getElementsByTagName('img')[0]
+
+  const imageOffsetLeft = image.offsetLeft
+  const imageOffsetTop = image.offsetTop
+
+  let moving = false
+
+  function move (direction) {
+    moving = true
+
+    fetch(`/api/move?direction=${direction}`, {
+      method: 'POST'
+    })
+  }
+
+  function stop () {
+    if (moving) {
+      moving = false
+
+      fetch('/api/stop', {
+        method: 'POST'
+      })
+    }
+  }
+
+  function fire () {
+    fetch('/api/fire', {
+      method: 'POST'
+    })
+  }
+
+  function handlePress (x, y) {
+    let xDirection
+    let yDirection
+
+    if (x > 310) {
+      xDirection = 1
+    } else if (x < 170) {
+      xDirection = 0
+    }
+
+    if (y > 370) {
+      yDirection = 1
+    } else if (y < 270) {
+      yDirection = 0
+    }
+
+    if (xDirection !== undefined) {
+      move(['left', 'right'][xDirection])
+    }
+
+    if (yDirection !== undefined) {
+      move(['up', 'down'][yDirection])
+    }
+  }
+
+  image.addEventListener('touchstart', event => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    for (const touch of event.touches) {
+      handlePress(touch.clientX - imageOffsetLeft, touch.clientY - imageOffsetTop)
+    }
+  })
+
+  image.addEventListener('dragstart', event => {
+    event.preventDefault()
+  })
+
+  image.addEventListener('touchmove', event => {
+    event.preventDefault()
+    event.stopPropagation()
+  })
+
+  image.addEventListener('touchend', event => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    stop()
+  })
+
+  image.addEventListener('touchcancel', event => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    stop()
+  })
+
+  image.addEventListener('mousedown', event => {
+    handlePress(event.offsetX, event.offsetY)
+  })
+
+  image.addEventListener('mouseup', () => {
+    stop()
+  })
+
+  image.addEventListener('contextmenu', event => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    fire()
+  })
+
+  window.addEventListener('keydown', event => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        move('left')
+
+        break
+
+      case 'ArrowRight':
+        move('right')
+
+        break
+
+      case 'ArrowUp':
+        move('up')
+
+        break
+
+      case 'ArrowDown':
+        move('down')
+    }
+  })
+
+  window.addEventListener('keyup', event => {
+    if (['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(event.key)) {
+      stop()
+    }
+  })
+}
