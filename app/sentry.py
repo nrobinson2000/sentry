f3e1589f66a029f0aa332e618bbaef4ea9ea0b43
Nathan Robinson <nrobinson2000@me.com>
2020-04-21 16:11:48 +0100

    Improve light state machine, auto off after 30 seconds

--- app/sentry.py
+++ app/sentry.py
@@ -5,9 +5,9 @@ import sys
 import time
 import usb.core
 import serial
+from threading import Timer
 
-
-class launchControl:
+class Sentry:
    def __init__(self):
       self.dev = usb.core.find(idVendor=0x2123, idProduct=0x1010)
 
@@ -23,6 +23,8 @@ class launchControl:
       if self.dev.is_kernel_driver_active(0) is True:
          self.dev.detach_kernel_driver(0)
       self.dev.set_configuration()
+
+      self.last_timer = Timer(30, self.light_off)
 
    def up(self):
       self.dev.ctrl_transfer(0x21,0x09,0,0,[0x02,0x02,0x00,0x00,0x00,0x00,0x00,0x00])
@@ -44,9 +46,15 @@ class launchControl:
 
    def light_on(self, r = 255, g = 255, b = 255):
        self.ser.write(f"{r},{g},{b}\n".encode("ascii"))
+       self.light_state = True
+       self.last_timer.cancel()
+       off_timer = Timer(30, self.light_off)
+       off_timer.start()
+       self.last_timer = off_timer
 
    def light_off(self):
        self.ser.write(b"0,0,0\n")
+       self.light_state = False
 
    def toggle_light(self):
        if self.light_state is False:
@@ -56,4 +64,4 @@ class launchControl:
            self.light_off()
            self.light_state = False
 
-sentry = launchControl()
+sentry = Sentry()
